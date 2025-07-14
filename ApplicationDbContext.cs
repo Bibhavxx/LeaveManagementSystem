c1e14efd1558d85c8f0b25785022d53cf23fd0e2
Bibhav Kumar Dubey <134066370+Bibhavxx@users.noreply.github.com>
2025-07-14 11:01:02 +0530

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LeavePortal.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using LeavePortal.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// using sql server 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Account}/{action=Login}/{id?}");
+
+app.Run();
